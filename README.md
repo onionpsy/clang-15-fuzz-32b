1a322dc3ffe96fab062187cebe73c64825d35617
psy <patrice.salathe@code-intelligence.com>
2023-06-29 12:30:46 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+```
+cd build
+   CXX=clang-15 CC=clang-15 cmake ..
+# or
+   CXX=clang-13 CC=clang-13 cmake ..
+make
+./fuzz
+```
---
+++ fuzz.cpp
@@ -0,0 +1,6 @@
+#include <cstdint> 
+#include <cstdio>
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+    return 0;
+}
